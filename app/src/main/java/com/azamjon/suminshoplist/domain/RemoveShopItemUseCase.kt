cdb78a390f70c4ab9d6df05e2d1248d171a2f038
Azamjon Ostonakulov <azamjonostonakulov@gmail.com>
2022-12-08 16:07:44 +0500

    Second step

---
+++ app/src/main/java/com/azamjon/suminshoplist/domain/RemoveShopItemUseCase.kt
@@ -0,0 +1,9 @@
+package com.azamjon.suminshoplist.domain
+
+import com.azamjon.suminshoplist.domain.model.ShopItem
+
+class RemoveShopItemUseCase {
+    fun removeShopItem(shopItem: ShopItem){
+        TODO()
+    }
+}
