d4ed5438616ac72a5d74ef1adb138e2628743dd0
Azamjon <azamjonostonakulov@gmail.com>
2023-05-03 02:26:28 +0500

    update

--- SuminShopList/app/src/main/java/com/azamjon/suminshoplist/presentation/adapter/ShopItemViewHolder.kt
+++
@@ -1,7 +0,0 @@
-package com.azamjon.suminshoplist.presentation.adapter
-
-import androidx.databinding.ViewDataBinding
-import androidx.recyclerview.widget.RecyclerView
-
-class ShopItemViewHolder(val binding: ViewDataBinding) :
-    RecyclerView.ViewHolder(binding.root)
